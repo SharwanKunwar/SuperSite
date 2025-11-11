9e77e7765e5970980bde9122b2feb5e87e73d3e2
SharwanKunwar <sharwankunwar07@gmail.com>
2025-11-11 09:43:42 +0545

    updated

--- super-site/src/pages/Explore.jsx
+++ super-site/src/pages/Explore.jsx
@@ -7,7 +7,7 @@ function Explore() {
       <div className="bg-[#101922] w-screen h-screen text-white flex justify-center items-start">
         <Container>
           <div className='bg-purple-400 h-[60px]'>navbar</div>
-          <div className='bg-yellow-400 h-[500px]'>contant</div>
+          <div className='bg-white h-[500px] flex justify-center items-center text-3xl font-bold text-black'>This page is in under construction.</div>
         </Container>
       </div>
     </>
